8c7380e5d37488df651163a8338918db7f34a032
student <sdoran35@gmail.com>
2015-09-02 09:47:35 -0500

    lkjm;l
    momkl;m

---
+++ src/com/company/Payroll.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+/**
+ * Created by student on 9/1/15.
+ */
+public class Payroll {
+    public static void main(String[]args){
+        double wage; // This is the regular wage.
+        double regHours; // This is the amount of hours worked.
+        double overHours;  // This is the amount of overtime hours worked.
+        double total; //This is the total weekly wage.
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Please enter your normal wage: ");
+        wage = reader.nextDouble();
+        System.out.print("Please enter the number of hours worked: ");
+        regHours = reader.nextDouble();
+        System.out.print("Please enter the number of overtime hours worked: ");
+        overHours = reader.nextDouble();
+        total = ((wage * regHours) + (wage * overHours));
+        System.out.print("Your total weekly wage is: "+ total);
+    }
+}
--- src/com/company/Round.java
+++ src/com/company/Round.java
@@ -10,11 +10,12 @@ public class Round {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         double input; //Input number
+        int output;  //Output Rounded number
 
 
         System.out.print("Enter the number to be rounded: ");
         input = reader.nextDouble();
-        int output = (int) (input + 0.5);
+        output = (int) (input + 0.5);
 
 
         System.out.print(output);
