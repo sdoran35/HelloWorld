324da1c571dd46fac4bb4dd9c2ab4533dbb4df87
student <sdoran35@gmail.com>
2015-09-23 09:48:13 -0500

    Weather and Time

--- src/com/company/FlippedImage.java
+++ src/com/company/FlippedImage.java
@@ -1,7 +1,46 @@
 package com.company;
 
+import images.APImage;
+import images.Pixel;
+
+import java.awt.*;
+import java.util.Scanner;
+
 /**
  * Created by student on 9/22/15.
  */
 public class FlippedImage {
+    public static void main(String[] args) {
+        // open up a keyboard scanner
+        Scanner reader = new Scanner(System.in);
+        APImage theOriginal = new APImage("/Users/student/OneDrive/AP Comp Sci/Media Computing/Smokey.jpg");
+        theOriginal.draw();
+        int width = theOriginal.getWidth();
+        int height = theOriginal.getHeight();
+        APImage theClone = new APImage(width, height);
+        int x, y;
+        int newX, newY;
+
+        for (x = 0; x < width; x++) {
+            for (y = 0; y < height; y++) {
+                Pixel pixel = theOriginal.getPixel(x, y);
+                int red = pixel.getRed();
+                int green = pixel.getGreen();
+                int blue = pixel.getBlue();
+                newX = width - x - 1;
+                newY = height - y - 1;
+                Pixel pixel1 = theClone.getPixel(newX, newY);
+                pixel1.setRed(red);
+                pixel1.setGreen(green);
+                pixel1.setBlue(blue);
+            }
+        }
+        System.out.print("Press Enter to Flip Image.");
+        reader.nextLine();
+        theClone.draw();
+    }
+
 }
+
+
+
--- src/com/company/MatrixCords.java
+++ src/com/company/MatrixCords.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.util.Scanner;
-
 /**
  * Created by student on 9/21/15.
  */
@@ -8,28 +7,26 @@ public class MatrixCords {
     public static void main (String []args ){
         Scanner reader = new Scanner(System.in);
 
-        int row;
-        int col;
-
         System.out.print("Enter the number of rows: ");
-         row = reader.nextInt();
+        int  row = reader.nextInt();
         System.out.print("Enter the number of columns: ");
-        col = reader.nextInt();
-
+       int col = reader.nextInt();
         System.out.println("The matrix coordinates in normal order: ");
-        for (  int y = 0; y < col; y++){
-            for (int x = 0; x < row; x++)
+        for (  int y = 0; y < row; y++){
+            for (int x = 0; x < col; x++)
                 System.out.print("(" + x + ", " + y + ") ");
                     System.out.println();
         }
-
         System.out.print("\n");
 
         System.out.println("The matrix coordinates in reverse order: ");
-        for (  int y = 0; y < col; y++){
-            for (int x = 0; x < row; x++)
+        for (  int y = row - 1; y >=0; y--){
+            for (int x = col - 1; x >= 0; x--)
                 System.out.print("(" + x + ", " + y + ") ");
             System.out.println();
+
+
+
         }
     }
 }
